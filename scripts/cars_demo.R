81af5f2d92c9a2873fae417731eba5f27e1c5540
Heather Mechler <hsmechler@unm.edu>
2024-01-24 14:59:04 -0700

    yep

--- scripts/cars_demo.R
+++ scripts/cars_demo.R
@@ -4,4 +4,15 @@ data("mtcars")
 
 View(mtcars)
 
+sport_cars<-mtcars %>% 
+  filter(cyl>=6)
 
+#4-step workflow
+#1. pull at least once when you start
+#2. add files to stage them for next snapshot
+#3. commit snapshots - this adds  snapshot to history
+#4. push commits to the remote (origin)
+
+summary(mtcars)
+
+plot(mtcars$mpg, mtcars$hp)
